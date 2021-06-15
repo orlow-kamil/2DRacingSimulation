b9de5aead05e5fab9484c65fdba64b25dc201e24
Kamil Orłow <orlow.kamil@gmail.com>
2021-06-15 21:14:11 +0200

    Create player prefab

---
+++ 2DRacingSimulation/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace RacingSimulation.Player
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private PlayerInput playerInput;
+        [SerializeField] private PlayerMover playerMover;
+
+        private Rigidbody2D rgbd;
+
+        private void Awake()
+        {
+            this.rgbd = this.GetComponent<Rigidbody2D>();
+            this.playerMover.Rgbd = this.rgbd;
+        }
+
+        private void FixedUpdate()
+        {
+            this.playerMover.Transform = this.transform;
+            this.playerMover.UpdateVelocity();
+
+            this.Accelarate();
+            this.Brake();
+            this.playerMover.Rotate(this.playerInput.GetHorizontalAxis());
+        }
+
+        private void Accelarate()
+        {
+            if (this.playerInput.OnAccelerate)
+            {
+                this.playerMover.Accelerate();
+            }
+        }
+
+        private void Brake()
+        {
+            if (this.playerInput.OnBrake)
+            {
+                this.playerMover.Brake();
+            }
+        }
+    }
+}
